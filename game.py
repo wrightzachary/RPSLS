7b47c85bd654fb4251726a93a14b09ce08d8b86d
wrightzachary <taylorzw96@gmail.com>
2021-06-15 12:25:53 -0500

    Added player.py and created the skeleton of the game on game.py

---
+++ game.py
@@ -0,0 +1,25 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def players(self):
+        pass
+        # determine if it is human vs AI or human vs human
+
+    def player_turn(self):
+        pass
+
+    def player2_turn(self):
+        pass
+
+    def computer_turn(self):
+        pass
+
+    def rounds(self):
+        pass
+
+    def display_winner(self):
+        pass
---
+++ player.py
@@ -0,0 +1,3 @@
+class Player:
+    def player(self):
+        pass
