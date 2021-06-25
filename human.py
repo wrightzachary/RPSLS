4dda926f2bdbd1b9b98f5fd4ed6bb63952dae623
wrightzachary <taylorzw96@gmail.com>
2021-06-24 23:32:28 -0500

    Human class complete

--- human.py
+++ human.py
@@ -1,10 +1,14 @@
+from player import Player
+
 class Human(Player):
     def __init__(self, name):
         super().__init__(name)
         self.name()
+        self.chosen_gesture = ""
 
     def set_name(self):
-        pass
+        self.chosen_gesture = ""
+
 
     def choose_gesture(self):
-        pass
+        self.name - input("Enter your name")
