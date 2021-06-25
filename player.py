2961bdc57c9be8478fac320a0a6fe26262fc8c91
wrightzachary <taylorzw96@gmail.com>
2021-06-24 23:39:53 -0500

    Player class made

--- player.py
+++ player.py
@@ -6,7 +6,16 @@ class Player:
         self.gestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
 
     def choose_gesture_player(self):
-        pass
+        gesture_input = input(f'{self.name} Enter gesture')
+        for gesture in self.gestures:
+            if gesture_input.lower() == gesture.lower():
+                self.chosen_gesture = gesture
+            else:
+                print(f'Input not valid, please retry')
+                self.choose_gesture_player()
 
     def show_gesture_options(self):
-        pass
+        gesture_index = 0
+        for each in self.gestures:
+            print(each)
+            gesture_index += 1
