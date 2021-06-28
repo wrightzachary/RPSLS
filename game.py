1c5cbb02d32dbb67f013c6288072b91fc0a104c0
wrightzachary <taylorzw96@gmail.com>
2021-06-28 00:35:04 -0500

    Player vs computer completed

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from human import Human
 
 player_one_score = 0
 player_two_score = 0
+computer_score = 0
 
 
 class Game:
@@ -14,8 +15,6 @@ class Game:
     def run_game(self):
         self.game_instructions()
         self.choose_game_mode()
-        self.game_logic()
-        self.best_out_of_three()
 
     @staticmethod
     def game_instructions():
@@ -42,27 +41,29 @@ class Game:
         response = int(input())
         if response == 1:
             self.name = input("Player one, please enter a name")
-            self.player_one_turn = input(f'Enter your gesture, {self.name}')
             self.player_two = Computer()
+            self.game_logic_single_player()
+            self.best_out_of_three_single_player()
         elif response == 2:
             self.player_one = Human(self)
             self.player_two = Human(self)
             self.name = input("Player one, please enter a name")
             self.name_other = input("Player two, please enter a name")
+            self.game_logic_multiplayer()
+            self.best_out_of_three_multiplayer()
         else:
             print("Not a valid number of players")
             Game.choose_game_mode(self)
         return
 
-    def game_logic(self):
+    def game_logic_multiplayer(self):
         global player_one_score
         global player_two_score
         self.player_one_turn = input(f'Enter your gesture, {self.name}')
         self.player_two_turn = input(f'Enter your gesture, {self.name_other}')
-        self.computer_turn = Computer.get_random_gesture(self.player_two)
-        if self.player_one_turn == self.player_two_turn or self.player_one_turn == self.computer_turn:
+        if self.player_one_turn == self.player_two_turn:
             print("Round tie!")
-        elif self.player_one_turn == "rock" and self.player_two_turn == "scissors" or self.computer_turn == "scissors":
+        elif self.player_one_turn == "rock" and self.player_two_turn == "scissors":
             print("Player_one wins the round!")
             print("The machine wins the round!")
             player_one_score += 1
@@ -98,10 +99,60 @@ class Game:
             player_two_score += 1
         return
 
-    def best_out_of_three(self):
+    def game_logic_single_player(self):
+        global player_one_score
+        global computer_score
+        self.player_one_turn = input(f'Enter your gesture, {self.name}')
+        self.computer_turn = Computer.get_random_gesture(self.player_two)
+        if self.player_one_turn == self.computer_turn:
+            print("Round tie!")
+        elif self.player_one_turn == "rock" and self.computer_turn == "scissors":
+            print("Player_one wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "paper" and self.computer_turn == "rock":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "scissors" and self.computer_turn == "paper":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "rock" and self.computer_turn == "lizard":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "lizard" and self.computer_turn != "spock":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "spock" and self.computer_turn == "scissors":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "scissors" and self.computer_turn == "lizard":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "lizard" and self.computer_turn == "paper":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "paper" and self.computer_turn == "spock":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        elif self.player_one_turn == "spock" and self.computer_turn == "rock":
+            print(f"{self.name} wins the round!")
+            player_one_score += 1
+        else:
+            print(f"The Machine wins the round!")
+            computer_score += 1
+        return
+
+    def best_out_of_three_multiplayer(self):
         while player_one_score < 2 and player_two_score < 2:
-            self.game_logic()
+            self.game_logic_multiplayer()
             if player_one_score == 2:
                 print(f'{self.name} wins!')
             else:
                 print(f'{self.name_other} wins!')
+
+    def best_out_of_three_single_player(self):
+        while player_one_score < 2 and computer_score < 2:
+            self.game_logic_single_player()
+            if player_one_score == 2:
+                print(f'{self.name} wins!')
+            else:
+                print(f'The Machine wins!')
